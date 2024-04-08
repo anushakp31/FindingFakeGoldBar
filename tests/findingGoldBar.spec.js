af66bb7adcbba8ad13b3d1a57d7d852aba3bf9e8
anushakp31 <anushakp31@gmail.com>
2024-04-08 15:12:58 -0500

    Increased Reusability

--- tests/findingGoldBar.spec.js
+++ tests/findingGoldBar.spec.js
@@ -1,41 +1,64 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 const _ = require('lodash');
-
-
 test('Algorithm', async ({ page }) => {
-
     const handleSuccessAlert = async () => {
         page.on('dialog', async dialog => {
             if (dialog.message() === "Yay! You find it!") {
                 console.log("Success message received: Yay! You find it!");
-                await dialog.accept(); // Accept the dialog
+                await dialog.accept();
             } else {
                 console.error("Unexpected dialog message:", dialog.message());
-              await expect(false).toBe(true); // Fails the test
+                await expect(false).toBe(true);
             }
         });
-        
+
     };
+    //Clicks on Reset button and asserts the sign
     const resetAndAssert = async () => {
         await page.getByRole('button', { name: 'Reset' }).click();
         await expect(resultSign).toContainText('?')
     };
 
-    const clickWeighButton = async () => {
+    //Clicks on Weigh button and returns a respective sign
+    const clickWeighButtonAndGetSign = async (resultSign) => {
         await page.locator('id=weigh').click();
+        await expect(resultSign).toContainText(/(<)|(>)|(=)/);
+        const initialSign = await resultSign.innerText();
+        return initialSign;
     };
 
     const fillBowls = async (leftValue, rightValue) => {
         await page.locator('id=left_0').fill(leftValue);
         await page.locator('id=right_0').fill(rightValue);
     };
-    const isResultSignEqualTo = async (expectedSign) => {
-        const resultSignText = await page.locator('button[id=reset][disabled]').innerText();
-        return resultSignText === expectedSign;
-    };
     const resultSign = page.locator('button[id=reset][disabled]')
-    
+    const handleComparisons = async (bowl) => {
+        console.log(`The fake bar is one among them! ${bowl}`);
+        await resetAndAssert();
+        fillBowls(bowl[0], bowl[1])
+        const initialSign1 = await clickWeighButtonAndGetSign(resultSign);
+        const olElement = await page.locator('ol');
+        const secondLiElementText = await olElement.locator('li:nth-child(2)').innerText();
+        console.log(secondLiElementText)
+        if (initialSign1 === '=') {
+
+            await handleSuccessAlert();
+            await page.locator(`id=coin_${bowl[2]}`).click();
+            console.log(`The fake bar is ${bowl[2]}`)
+
+        }
+        else if (initialSign1 === '<') {
+            await handleSuccessAlert();
+            await page.locator(`id=coin_${bowl[0]}`).click();
+            console.log(`The fake bar is ${bowl[0]}`)
+        }
+        else {
+            await handleSuccessAlert();
+            await page.locator(`id=coin_${bowl[1]}`).click();
+            console.log(`The fake bar is ${bowl[1]}`)
+        }
+    }
 
     await page.goto('http://sdetchallenge.fetch.com/');
 
@@ -54,111 +77,36 @@ test('Algorithm', async ({ page }) => {
     console.log('Left Bowl:', leftBowl);
     console.log('Right Bowl:', rightBowl);
     console.log('Remainder:', remainder);
-   
+
 
     await expect(page.getByText('Weighings')).toBeVisible();
     for (let i = 0; i < leftBowl.length; i++) {
         await page.locator(`id=left_${i}`).fill(leftBowl[i]);
     }
-    console.log("Filled Left")
+    // console.log("Filled Left")
 
     for (let i = 0; i < leftBowl.length; i++) {
         await page.locator(`id=right_${i}`).fill(rightBowl[i]);
     }
 
-    console.log("Filled Right")
 
-    clickWeighButton();
-    await expect(resultSign).toHaveText(/(<)|(>)|(=)/);
-    const initialSign=await resultSign.innerText();
-
-    if (initialSign==='=') {
+    // console.log("Filled Right")
+    const initialSign = await clickWeighButtonAndGetSign(resultSign);
+    const olElement = await page.locator('ol');
+  const text = await olElement.innerText();
+  console.log(text)
+    if (initialSign === '=') {
         console.log("The left and the right bowls are equal!!")
-        await page.getByRole('button', { name: 'Reset' }).click();
-        await expect(resultSign).toContainText('?')
-       fillBowls(remainder[0],remainder[1])
-        clickWeighButton();
-        await expect(resultSign).toHaveText(/(<)|(>)|(=)/);
-        const initialSign1=await resultSign.innerText();
-
-        if (initialSign1==='=') {
-
-            handleSuccessAlert();
-            await page.locator(`id=coin_${remainder[2]}`).click();
-            console.log(`The fake bar is ${remainder[2]}`)
-
-        }
-        else if (initialSign1==='<') {
-            handleSuccessAlert();
-            await page.locator(`id=coin_${remainder[0]}`).click();
-           
-            console.log(`The fake bar is ${remainder[0]}`)
-        }
-        else {
-            handleSuccessAlert();
-            await page.locator(`id=coin_${remainder[1]}`).click();
-            
-            console.log(`The fake bar is ${remainder[1]}`)
-        }
+        await handleComparisons(remainder);
     }
-    else if(initialSign==='<'){
-            
-            console.log("The left bowl is lesser than the right bowl!!")
-            await page.getByRole('button', { name: 'Reset' }).click();
-            await expect(resultSign).toContainText('?')
-            fillBowls(leftBowl[0],leftBowl[1])
-            clickWeighButton();
-            await expect(resultSign).toHaveText(/(<)|(>)|(=)/);
-            const initialSign1=await resultSign.innerText();
-    
-            if (initialSign1==='=') {
-                
-                handleSuccessAlert();
-                await page.locator(`id=coin_${leftBowl[2]}`).click();
-               
-                console.log(`The fake bar is ${leftBowl[2]}`)
+    else if (initialSign === '<') {
 
-            }
-            else if (initialSign1==='<') {
-                handleSuccessAlert();
-                await page.locator(`id=coin_${leftBowl[0]}`).click();
-                
-                console.log(`The fake bar is ${leftBowl[0]}`)
-            }
-            else {
-                handleSuccessAlert();
-                await page.locator(`id=coin_${leftBowl[1]}`).click();
-                
-                console.log(`The fake bar is ${leftBowl[1]}`)
-            }
-        }
-        else if(initialSign==='>'){
-            console.log("The right bowl is lesser than the left bowl!!")
-            await page.getByRole('button', { name: 'Reset' }).click();
-            await expect(resultSign).toContainText('?')
-            fillBowls(rightBowl[0],rightBowl[1])
-            clickWeighButton();
-            await expect(resultSign).toHaveText(/(<)|(>)|(=)/);
-            const initialSign1=await resultSign.innerText();
-            if (initialSign1==='=') {
-                await page.locator(`id=coin_${rightBowl[2]}`).click();
-                handleSuccessAlert();
-                console.log(`The fake bar is ${rightBowl[2]}`)
+        console.log("The left bowl is lesser than the right bowl!!")
+        await handleComparisons(leftBowl)
+    }
+    else if (initialSign === '>') {
+        console.log("The right bowl is lesser than the left bowl!!")
+        await handleComparisons(rightBowl)
+    }
 
-            }
-            else if (initialSign1==='<') {
-                handleSuccessAlert()
-                await page.locator(`id=coin_${rightBowl[0]}`).click();
-                ;
-                console.log(`The fake bar is ${rightBowl[0]}`)
-            }
-            else {
-                handleSuccessAlert();
-                await page.locator(`id=coin_${rightBowl[1]}`).click();
-               
-                console.log(`The fake bar is ${rightBowl[1]}`)
-            }
-
-        }
-    
 });
